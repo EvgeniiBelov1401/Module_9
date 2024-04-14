7fb36361949e2936829c63387db545fd03f9c4b5
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-14 18:10:42 +0300

    Добавил Exercise_9_1_04

---
+++ Module_9/Exercise_9_1_04/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_9_1_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception =new Exception("Ошибка");
+            exception.HelpLink = "www.google.com";
+        }
+    }
+}
