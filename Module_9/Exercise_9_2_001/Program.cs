1e9df48928948ce5853898c947919261df25c44a
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-14 19:07:51 +0300

    Добавил Exercise_9_2_001

---
+++ Module_9/Exercise_9_2_001/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise_9_2_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Блок try отработал");
+                throw new FileNotFoundException();
+            }
+            catch (Exception ex) when(ex is ArgumentNullException)
+            {
+
+                Console.WriteLine("Аргумент пустой");
+                
+            }
+            catch (Exception ex) when (ex is FileNotFoundException)
+            {
+
+                Console.WriteLine("Файл не найден");
+
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally отработал");
+            }
+        }
+    }
+}
