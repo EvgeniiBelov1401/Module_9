0e14f0326d6c609dd6bed203cfa815ca13e502f8
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-14 19:22:38 +0300

    Добавил Exercise_9_2_03

---
+++ Module_9/Exercise_9_2_03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_9_2_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new RankException();
+            }
+            catch (RankException ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
