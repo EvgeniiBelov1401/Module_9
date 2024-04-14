84c1e79912ea13fff519a676ada2e22a53e7b5f2
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-14 18:03:04 +0300

    Добавил Exercise_9_1_03

---
+++ Module_9/Exercise_9_1_03/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise_9_1_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception();
+
+            exception.Data.Add("Дата ошибки",DateTime.Now);
+        }
+    }
+}
