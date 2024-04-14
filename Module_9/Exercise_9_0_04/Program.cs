f00eaaf6c41998c9b19fa9b0dcc03e91cead779c
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-14 17:33:33 +0300

    Добавил Exercise_9_0_04

---
+++ Module_9/Exercise_9_0_04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise_9_0_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"D:\Programming\Skillfactory\C#_projects\Module_9\Module_9\Exercise_9_0_04\Новая папка");
+            if (!dirInfo.Exists )
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory("Внутренняя папка");
+        }
+    }
+}
