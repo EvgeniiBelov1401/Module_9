a46b5c4ca30332fc0bee3fe176d52e2741fe507c
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-14 19:17:22 +0300

    Добавил Exercise_9_2_02

---
+++ Module_9/Exercise_9_2_02/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_9_2_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                throw new ArgumentOutOfRangeException("Значение не попало в интервал значений");
+            }
+            catch(ArgumentOutOfRangeException ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("...");
+            }
+        }
+    }
+}
