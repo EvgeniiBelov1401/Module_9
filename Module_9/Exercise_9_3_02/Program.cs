2792ec83032d1c1b533e3061e780ad9bc59dcb0d
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-15 21:28:26 +0300

    Добавил Exercise_9_3_02

---
+++ Module_9/Exercise_9_3_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise_9_3_02
+{
+    internal class Program
+    {
+        delegate int SubtractionDelegate(int a,int b);
+        static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+            SubtractionDelegate subtractionDelegate = Subtraction;
+
+            int result = subtractionDelegate.Invoke(50,20);
+            Console.WriteLine(result);  
+            
+        }
+    }
+}
