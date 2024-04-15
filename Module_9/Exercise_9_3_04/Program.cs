dee348a9c5167d1a891f317bede6d02068c6d6ee
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-15 21:52:49 +0300

    Добавил Exercise_9_3_04

---
+++ Module_9/Exercise_9_3_04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise_9_3_04
+{
+    internal class Program
+    {
+        delegate void ShowDelegate(int a,int b);
+
+        static void Subtraction(int a,int b)
+        {
+            Console.WriteLine($"{a}-{b}={ a - b}");
+        }
+        static void Sum(int a,int b)
+        {
+            Console.WriteLine($"{a}+{b}={a+b}");
+        }
+
+        static void Main(string[] args)
+        {
+            ShowDelegate showDelegate = Subtraction;
+            showDelegate += Sum;
+            showDelegate.Invoke(10,7);
+            
+        }
+    }
+}
