e83f63d66466cb5d5ff21b564c2667313dc20844
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-15 22:16:29 +0300

    Проверил, как работает объединение делегатов

---
+++ Module_9/Exercise_9_3_006/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_9_3_006
+{
+    internal class Program
+    {
+        delegate void ShowDelegate(int a,int b);
+
+        static void Subtraction(int a,int b)
+        {
+            Console.WriteLine($"{a}-{b}={a-b}");
+        }
+        static void Sum(int a, int b)
+        {
+            Console.WriteLine($"{a}+{b}={a + b}");
+        }
+        static void Main(string[] args)
+        {
+            ShowDelegate showDelegate1 = Subtraction;
+            ShowDelegate showDelegate2 = Sum;
+            ShowDelegate showDelegate3=showDelegate1 + showDelegate2;
+            showDelegate3.Invoke(30,6);
+        }
+    }
+}
