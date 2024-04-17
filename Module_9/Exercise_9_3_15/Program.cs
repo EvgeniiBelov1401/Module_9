0315605b3a97aea2b58359ca8390ede4e587527a
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-17 14:35:01 +0300

    Добавил Exercise_9_3_15 - анономная функция через лямбда-выражение

---
+++ Module_9/Exercise_9_3_15/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise_9_3_15
+{
+    internal class Program
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate=() => new Random().Next(0, 100);
+            
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+        }
+    }
+}
