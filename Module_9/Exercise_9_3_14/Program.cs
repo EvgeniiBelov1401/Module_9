d95d5ac633c3e6e45990b53e2575a786eac6a71b
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-17 14:21:53 +0300

    Добавил Exercise_9_3_14 - анонимный метод через лямбда-выражение

---
+++ Module_9/Exercise_9_3_14/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise_9_3_14
+{
+    internal class Program
+    {
+        delegate void ShowMessageDelegate(string _message);
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate showMessageDelegate = message=> Console.WriteLine(message);        
+            showMessageDelegate.Invoke("Hello World!");
+        }
+    }
+}
