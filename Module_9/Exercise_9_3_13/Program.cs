637572f3d62ea0228e546270452f70edd4bdc07d
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-17 14:16:32 +0300

    Добавил Exercise_9_3_13 - анонимная функция

---
+++ Module_9/Exercise_9_3_13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise_9_3_13
+{
+    internal class Program
+    {
+        delegate int RandomNumberDelegate();
+        //static int RandomNumber()
+        //{
+        //    return new Random().Next(0, 100);
+        //}
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = delegate
+            {
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+        }
+    }
+}
