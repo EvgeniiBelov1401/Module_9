2c4ea2785be4d9fc534412e2780d32a49f3ca234
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-17 16:45:46 +0300

    Добавил Exercise_9_4_02 - ковариантность

---
+++ Module_9/Exercise_9_4_02/Models/Lexus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9_4_02.Models
+{
+    internal class Lexus:Car
+    {
+
+    }
+}
---
+++ Module_9/Exercise_9_4_02/Program.cs
@@ -0,0 +1,23 @@
+﻿using Exercise_9_4_02.Models;
+
+namespace Exercise_9_4_02
+{
+    internal class Program
+    {
+        delegate Car GetCarInfo();
+        static Car CarInfo()
+        {
+            return new Car();
+        }
+        static Lexus LexusInfo()
+        {
+            return new Lexus();
+        }
+        static void Main(string[] args)
+        {
+            GetCarInfo getCarInfo1 = CarInfo;
+            GetCarInfo getCarInfo2 = LexusInfo;
+            
+        }
+    }
+}
